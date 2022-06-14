583dae5384bb2a6f8710af652a4c2ffe181ba444
JoyleneKirui <joylene.kirui@student.moringaschool.com>
2022-06-14 23:04:44 +0300

    add settings

--- tuzo/settings.py
+++ tuzo/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
+    'bootstrap3'
     'awwards',
     'cloudinary',
     'rest_framework',
